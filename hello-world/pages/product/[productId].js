3d3e28264920ba60d9198b7416837d5fe53a73a2
Gaurav Yadav <gk5480325@gmail.com>
2022-07-17 14:58:00 +0530

    DyanimcbRoute

--- hello-world/pages/product/[productId].js
+++ hello-world/pages/product/[productId].js
@@ -1,5 +1,9 @@
+import { useRouter } from 'next/router';
+
 function ProductDetail() {
-    return <h1>Details about Product</h1>
+    const router = useRouter();
+    const productId = router.query.productId;
+    return <h1>Details about Product { productId }</h1>
 }
 
 export default ProductDetail;
