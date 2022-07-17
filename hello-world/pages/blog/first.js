7a58fafaa37fdb38a7e8254024b56dcec7e640a8
Gaurav Yadav <gk5480325@gmail.com>
2022-07-17 14:26:20 +0530

    Nested Route first and second blog done

---
+++ hello-world/pages/blog/first.js
@@ -0,0 +1,5 @@
+function FirstBlog() {
+    return <h1>FirstBlog</h1>
+}
+
+export default FirstBlog;
---
+++ hello-world/pages/blog/second.js
@@ -0,0 +1,5 @@
+function SecondBlog() {
+    return <h1>Second Blog</h1>
+}
+
+export default SecondBlog;
