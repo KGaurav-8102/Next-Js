d288deaf463a0e35c5ecdd14f5dff96e33ffc65b
Gaurav Yadav <gk5480325@gmail.com>
2022-07-17 14:51:44 +0530

    Dyanmics Route Started

---
+++ hello-world/pages/product/[productId].js
@@ -0,0 +1,5 @@
+function ProductDetail() {
+    return <h1>Details about Product</h1>
+}
+
+export default ProductDetail;
---
+++ hello-world/pages/product/index.js
@@ -0,0 +1,11 @@
+function Product() {
+    return (
+        <>
+            <h1>Product 1</h1>
+            <h1>Product 2</h1>
+            <h1>Product 3</h1>
+        </>
+    );
+}
+
+export default Product;
