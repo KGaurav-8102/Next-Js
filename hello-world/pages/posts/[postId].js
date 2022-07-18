2d62bb56fc49ade879e7bd14b261fce117807d5a
Gaurav Yadav <gk5480325@gmail.com>
2022-07-18 14:51:16 +0530

    SSR DOne

--- hello-world/pages/posts/[postId].js
+++ hello-world/pages/posts/[postId].js
@@ -1,13 +1,10 @@
-
 function Post({ post }) {
     return(
         <>
-            <h2>
-                {post.id} {post.title}
-            </h2>
+            <h2>{post.id} {post.title}</h2>
             <p>{post.body}</p>
         </>
-    );
+    )
 }
 
 export default Post;
@@ -15,7 +12,7 @@ export default Post;
 export async function getStaticProps(context) {
     const { params } = context
     const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${params.postId}`)
-    const data = await response.json();
+    const data = await response.json()
 
     return {
         props: {
--- hello-world/pages/posts/index.js
+++ hello-world/pages/posts/index.js
@@ -1,34 +1,37 @@
 import Link from 'next/link';
 
-function PostList({ posts }) {
+function PostList({ posts })  {
     return (
         <>
-           <h1>List of Posts</h1>
-           {posts.map(post => {
-                return(
-                    <div key={post.id}>
-                        <Link href={`posts/${post.id}`}>
-                            <h2>
-                                {post.id} {post.title}
-                            </h2>
-                        </Link>
-                        <hr />
-                    </div>
-                )
-           })}
+            <h1>List of Posts</h1>
+            {
+                posts.map(post => {
+                    return (
+                        <div key={post.id}>
+                            <Link href={`posts/${post.id}`} passHref>
+                                <h2>
+                                    {post.id} {post.title}
+                                </h2>
+                            </Link>
+                            <hr />
+                        </div>
+                    )
+                })
+            }
         </>
-    )
+    );
 }
 
 export default PostList;
 
+
 export async function getStaticProps() {
-    const response = await fetch('https://jsonplaceholder.typicode.com/posts');
-    const data = await response.json();
+    const response = await fetch('https://jsonplaceholder.typicode.com/posts')
+    const data = await response.json()
 
     return {
         props: {
-            posts: data.slice(0, 3),
-        },
+            posts: data.slice(0, 3)
+        }
     }
 }
