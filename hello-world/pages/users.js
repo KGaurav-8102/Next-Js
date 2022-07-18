25ced18dd52bdb65607d37f08b96184a527c4991
Gaurav Yadav <gk5480325@gmail.com>
2022-07-18 09:26:35 +0530

    users are generated by GetStatic Props

---
+++ hello-world/pages/users.js
@@ -0,0 +1,31 @@
+import User from '../components/user';
+
+function UserList({ users })  {
+    return (
+    <>
+        <h1>UserList</h1>
+        {users.map((user) => {
+                return (
+                    <div key={user.id}>
+                        <User user={user} />
+                    </div>
+                )
+            })
+        }
+    </>
+);
+}
+
+export default UserList
+
+export async function getStaticProps() {
+    const response = await fetch('https://jsonplaceholder.typicode.com/users')
+    const data = await response.json()
+    console.log(data)
+
+    return {
+        props: {
+            users: data,
+        },
+    }
+}
