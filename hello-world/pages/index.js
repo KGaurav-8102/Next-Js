89cb20cd3d43a6c402662aacc5fb224a3869d3fa
Gaurav Yadav <gk5480325@gmail.com>
2022-07-18 14:07:00 +0530

    SSG with Dynamics Parameter

--- hello-world/pages/index.js
+++ hello-world/pages/index.js
@@ -1,7 +1,15 @@
-
+import Link from 'next/link';
 
 function App() {
-    return <h1>NextJs Pre-rendering</h1>
+    return <>
+        <h1>NextJs Pre-rendering</h1>
+        <Link href='/users'>
+            <a>Users</a>
+        </Link>
+        <Link href='/posts'>
+            <a>Posts</a>
+        </Link>
+    </>
 }
 
 export default App;
