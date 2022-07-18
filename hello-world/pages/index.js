d1b2baaa3e182aaacbaabc519429428dd89a27af
Gaurav Yadav <gk5480325@gmail.com>
2022-07-18 09:26:12 +0530

    getStatic Props Started

--- hello-world/pages/index.js
+++ hello-world/pages/index.js
@@ -1,25 +1,7 @@
-import Link from 'next/link';
-import { useRouter } from 'next/router';
 
-function Home() {
-    const router = useRouter();
-    const handleClick = () => {
-        console.log('order has been placed Successfully');
-        router.push('/product');
-    }
 
-    return(
-        <div>
-            <h1>HomePage</h1>
-            <Link href='/blog'>
-                <a>Blog</a>
-            </Link>
-            <Link href='/product'>
-                <a>Products</a>
-            </Link>
-            <button onClick={handleClick}>Place Order</button>
-        </div>
-    );
+function App() {
+    return <h1>NextJs Pre-rendering</h1>
 }
 
-export default Home;
+export default App;
