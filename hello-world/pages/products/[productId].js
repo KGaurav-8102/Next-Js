f515c0c737b664327cb3058041320b5d0cfaa180
Gaurav Yadav <gk5480325@gmail.com>
2022-07-19 18:02:51 +0530

    incremental Static Rerendering

---
+++ hello-world/pages/products/[productId].js
@@ -0,0 +1,40 @@
+import { useRouter } from 'next/router';
+
+function Product({ product }) {
+    const router = useRouter()
+
+    if (router.isFallback) {
+        return <div>Loading ....</div>
+    }
+
+    return (
+        <div>
+            <h2>{product.id} {product.title} {product.price}</h2>
+            <p>{product.description}</p>
+            <hr />
+        </div>
+    )
+}
+
+export default Product
+
+
+export async function getStaticProps(context) {
+    const { params } = context
+
+    const response = await fetch(`http://localhost:4000/products/${params.productId}`)
+    const data = await response.json()
+    
+    return {
+        props: {
+            product : data
+        }
+    }
+}
+
+export async function getStaticPaths() {
+    return {
+        paths: [{ params: { productId: '1'}}],
+        fallback: true
+    }
+}
