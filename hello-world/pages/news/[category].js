55f12dba3c6d8d532c1031f894acbf0f8bf2a3f1
Gaurav Yadav <gk5480325@gmail.com>
2022-07-22 08:38:30 +0530

    Cookies has done

--- hello-world/pages/news/[category].js
+++ hello-world/pages/news/[category].js
@@ -1,40 +1,41 @@
-function ArticleListByCategory({ articles, category }) {
+export default function ArticlesListByCategory({ articles, category }) {
     return (
-      <>
-        <h1>Showing news for category "{category}"</h1>
-        {articles.map(article => {
-          return (
-            <div key={article.id}>
-              <h2>
-                {article.id} {article.title}
-              </h2>
-              <p>{article.description}</p>
-              <hr />
-            </div>
-          )
-        })}
-      </>
+        <>
+             <h1>Showing news for category "{category}"</h1>
+            {articles.map(article => {
+                return (
+                <div key={article.id}>
+                    <h2>
+                    {article.id} {article.title}
+                    </h2>
+                    <p>{article.description}</p>
+                    <hr />
+                </div>
+                )
+            })}
+        </>
     )
-  }
-  
-  export default ArticleListByCategory
-  
-  export async function getServerSideProps(context) {
-    const { params, req, res, query } = context
+}
+
+export async function getServerSideProps(context) {
+
+    const { params, req, res } = context
+    console.log(req.headers.cookie);
+    res.setHeader('set-Cookie', ['name=Gaurav'])
     const { category } = params
+
     const response = await fetch(
-      `http://localhost:4000/news?category=${category}`
+        `http://localhost:4000/news?category=${category}`
     )
+
     const data = await response.json()
-  
-    console.log(`Pre-rendering News Articles for category ${category}`)
-    res.setHeader('Set-Cookie', ['name=Vishwas'])
-    console.log(req.headers.cookie)
-    console.log(query)
+    console.log(data);
     return {
-      props: {
-        articles: data,
-        category
-      }
+        props: {
+            articles: data,
+            category,
+        }
     }
-  }
+
+
+}
