f4d4f33b4d6943c7e2c2e253e41800ee97f47e08
Gaurav Yadav <gk5480325@gmail.com>
2022-07-23 08:26:21 +0530

    Dynamic API routes ius going on

---
+++ next-api/data/comments.js
@@ -0,0 +1,15 @@
+
+export const comments = [
+    {
+        "id" : 1,
+        "text" : "this is my first comment"
+    },
+    {
+        "id" : 2,
+        "text" : "this is my Second comment"
+    },
+    {
+        "id" : 3,
+        "text" : "this is my third comment"
+    }
+]
