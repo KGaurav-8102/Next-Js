1e3fdb5b4846f9c73efb484adc459faf6b714869
Gaurav Yadav <gk5480325@gmail.com>
2022-07-24 18:00:23 +0530

    Head Component started

--- next-misc/pages/About.js
+++ next-misc/pages/About.js
@@ -1,8 +1,15 @@
+import Head from 'next/head'
 import Footer from  '../components/Footer'
 
 function About() {
     return (
-        <h1 className='content'>About Page is Simple</h1>
+        <>
+            <Head>
+               <title>Code KgSoftech</title>
+               <meta name='description' content='free tutorials on web development' />
+            </Head>
+            <h1 className='content'>About Page is Simple</h1>
+        </>
     )
 }
 
