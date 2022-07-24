aa2385a9423d2fcbae0586e7a589bf9b75acb640
Gaurav Yadav <gk5480325@gmail.com>
2022-07-24 19:18:15 +0530

    Head Component Completed

--- next-misc/pages/_app.js
+++ next-misc/pages/_app.js
@@ -1,3 +1,4 @@
+import Head from 'next/head'
 import Footer from '../components/Footer'
 import Header from '../components/Header'
 import '../styles/globals.css'
@@ -11,6 +12,10 @@ function MyApp({ Component, pageProps }) {
 
   return(
     <>
+      <Head>
+        <title>KgSoftech</title>
+        <meta name='description' content='free tutorials for web development' />
+      </Head>
       <Header />
       <Component {...pageProps} />
       <Footer />
