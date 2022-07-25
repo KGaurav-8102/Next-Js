cef2f77e714180ce44fe80e73ee0991a9b8ed8d4
Gaurav Yadav <gk5480325@gmail.com>
2022-07-25 19:00:40 +0530

    Oauth Flow of Github Added

---
+++ next-auth/pages/api/auth/[...nextauth].js
@@ -0,0 +1,13 @@
+import NextAuth from 'next-auth'
+import Providers from 'next-auth/providers'
+
+export default NextAuth({
+    providers: [
+        Providers.Github({
+            clientId: '',
+            clientSecret: '',
+        })
+    ]
+})
+
+
